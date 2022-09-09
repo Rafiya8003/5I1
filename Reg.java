6a4f3ef5bbdb0d8925aec32d53c0277cbf270356
Rafiya Sultana <patanrafiyasultana03@gmail.com>
2022-09-09 11:42:39 +0530

    9/9

---
+++ Reg.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.sql.*;
+import java.servlet.http.HttpServlet;
+import java.servlet.http.HttpServletRequest;
+import java.servlet.http.HttpServletResponse;
+public class Reg
+{
+    public static void doGet(HttpServletRequest req, HttpServletResponse res ) {
+        
+    }
+}
